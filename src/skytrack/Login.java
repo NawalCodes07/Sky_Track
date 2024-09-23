41e3e4fd12affbbbcee3e1bfcc977fed63e6dabd
Nawal <nawalsmnautiyal@gmail.com>
2024-09-23 22:48:23 +0530

    Initial commit

---
+++ src/skytrack/Login.java
@@ -0,0 +1,16 @@
+
+package skytrack;
+
+import javax.swing.*;
+
+public class Login extends JFrame {
+    
+    public Login() {
+        setSize(400, 300);
+        setLocation(700, 250);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new Login();
+    }
+}
